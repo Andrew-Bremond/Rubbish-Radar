12084f3fccf84b1e9f15c2c32cf65a6e9b840b64
arjunbhalla03 <arjunbhalla03@gmail.com>
2023-08-15 17:45:07 +0900

    Adding a map JS function

--- src/main.js
+++ src/main.js
@@ -5,14 +5,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import * as VueGoogleMaps from "vue2-google-maps";
-
-Vue.use(VueGoogleMaps, {
-    load: {
-        key: "AIzaSyBXgokapngeS3mM4RaZfYr8fCIvKshSdzk"
-    },
-    installComponents: true,
-});
 
 const app = createApp(App)
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
   import { RouterLink, RouterView } from 'vue-router'
+  import * as VueGoogleMaps from "vue2-google-maps";
 </script>
 
 <template>
@@ -8,6 +9,15 @@
     <p> Theoretically, elements should be consistent routing to different pages. </p>
   </header>
 </template>
+
+<script>
+  Vue.use(VueGoogleMaps, {
+    load: {
+        key: "AIzaSyBXgokapngeS3mM4RaZfYr8fCIvKshSdzk",
+        libraries: "places"
+    }
+  });
+</script>
 
 <style scoped>
 </style>
