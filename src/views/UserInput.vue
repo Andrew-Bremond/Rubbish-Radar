c11e324fa9d8c7fc4539209a92b7774fe3daca43
Kush <kushmomaya@gmail.com>
2023-08-15 22:57:12 +0900

    html is in for user input page

---
+++ src/views/UserInput.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <body>
+        <div>
+            <h1>Add Trash Can</h1>
+            <br>
+            <input v-model="additionalInfo" placeholder="Info About Trash Can Location">
+            <br>p
+            <button @click="addTrashCan">Add Current Location</button>
+        </div>
+    </body>
+</template>
+
+<style>
+    body {
+        text-align: center;
+    }
+
+</style>
+
+
+<script>
+    import { ref } from 'vue';
+
+    const additionalInfo = ref('');
+
+    const addTrashCan = () => {
+        
+    }
+
+
+</script>
