1ca5b7c76dc0ac88cc0e70feface99cc3cce2935
Kush <kushmomaya@gmail.com>
2023-08-17 16:56:22 +0900

    working get position button

--- src/views/UserInput.vue
+++ src/views/UserInput.vue
@@ -3,15 +3,14 @@
 </script>
 
 <template>
-    <body>
-        <div>
-            <h1>Add Trash Can</h1>
-            <br>
-            <input v-model="additionalInfo" placeholder="Info About Trash Can Location">
-            <br>p
-            <button @click="addTrashCan">Add Current Location</button>
-        </div>
-    </body>
+    <div class="userInput">
+        <h1>Add Trash Can</h1>
+        <br>
+        <input v-model="additionalInfo" placeholder="Info About Trash Can Location">
+        <br>
+        <button @click="addTrashCan">Add Current Location</button>
+        <p v-if="location">Trash can added at location: {{location.latitude}}, {{location.longitude}}</p>
+    </div>
 </template>
 
 <style>
@@ -25,17 +24,34 @@
 <script>
     import { ref } from 'vue';
 
-    data(){
-        return {
-            additionalInfor: '',
-        }
-    },
-    methods: {
-        
-    }
-    const addTrashCan = () => {
-        
-    }
+    export default {
+        data(){
+            return {
+                additionalInfo: '',
+                location: null,
+            };
+        },
+        methods: {
+            addTrashCan(){
+                if("geolocation" in navigator){
+                    navigator.geolocation.getCurrentPosition(
+                        position => {
+                            this.location = {
+                                latitude: position.coords.latitude,
+                                longitude: position.coords.longitude,
+                            };
+                        },
+                        error => {
+                            console.error("Error getting location: ", error.message)
+                        }
+                    );
+                } else {
+                    console.error("Location services not available in this browser")
+                }
+            },
+        },
+    };
+    
 
 
 </script>
