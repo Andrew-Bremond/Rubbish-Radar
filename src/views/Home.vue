0ac6309e442eeec9cdde492472da67447e887064
Tristan C <torisutansan02@gmail.com>
2023-08-22 10:00:00 +0900

    removing unnecessary methods:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,6 +21,13 @@
     methods: {
       goToUserInputPage() {
         this.$router.push("/UserInput");
+      },
+      initMap() {
+        let mapOptions = {
+          center: { lat: 41, lng: -87 },
+          zoom: 12
+        }
+        let map = new google.maps.Map(document.getElementById('map'), mapOptions); 
       }
     },
 
@@ -33,15 +40,6 @@
         script.onload = this.initMap;
       } else {
         this.initMap();
-      }
-    },
-    methods: {
-      initMap() {
-        let mapOptions = {
-          center: { lat: 41, lng: -87 },
-          zoom: 12
-        }
-        let map = new google.maps.Map(document.getElementById('map'), mapOptions); 
       }
     },
   }
