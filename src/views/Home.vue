b91f53f17f919266b3d987316af2713f041cf152
Tristan C <torisutansan02@gmail.com>
2023-08-23 09:52:53 +0900

    Please accept pull request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,11 +25,44 @@
       initMap() {
         let mapOptions = {
           center: { lat: 41, lng: -87 },
-          zoom: 8
+          zoom: 16
         }
         let map = new google.maps.Map(document.getElementById('map'), mapOptions); 
-      }
-    },
+
+        let infoWindow = new google.maps.InfoWindow();
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("Location found.");
+            infoWindow.open(map);
+            map.setCenter(pos);
+            },
+              () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            },
+          );
+          } else {
+            handleLocationError(false, infoWindow, map.getCenter());
+          }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+        infoWindow.setContent(
+          browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation.",
+      );
+      infoWindow.open(map);
+    }
+    window.initMap = initMap;
+  }},
 
     name: 'MapTest',
     mounted() {
